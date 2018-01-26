c67b9b38eaf041c0099da30636930ac632d0b55f
hujia <hujiay@stanford.edu>
2018-01-26 14:03:03 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,6 @@ from skimage.exposure import rescale_intensity
 # returns image of shape [224, 224, 3]
 # [height, width, depth]
 def load_image(path):
-    # load image
     img = skimage.io.imread(path)
     # img = Image.open(path).convert('LA') # convert to grayscale
     # im = imread(im_path)
@@ -20,14 +19,10 @@ def load_image(path):
     img = rescale_intensity(img, out_range=(0, 255))
     # scipy.misc.imsave('./test.png', img)
     # img = img / 255.0
-    # assert (0 <= img).all() and (img <= 1.0).all()
-    # print "Original Image Shape: ", img.shape
-    # we crop image from center
     short_edge = min(img.shape[:2])
     yy = int((img.shape[0] - short_edge) / 2)
     xx = int((img.shape[1] - short_edge) / 2)
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
-    # resize to 224, 224
     resized_img = skimage.transform.resize(crop_img, (224, 224))
     return resized_img
 
@@ -36,16 +31,10 @@ def load_image(path):
 def print_prob(prob, file_path):
     gender = [l.strip() for l in open(file_path).readlines()]
 
-    # print prob
     pred = np.argsort(prob)[::-1]
-    # print pred.shape
-    # print gender
-    # Get top1 label
     top1 = gender[pred[0]]
-    # print((top1, prob[pred[0]]))
     # Get top5 label
     # top5 = [(synset[pred[i]], prob[pred[i]]) for i in range(5)]
-    # print(("Top5: ", top5))
     return top1
 
 
